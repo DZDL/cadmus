ee67f8d0fd38eecd812c5203e4384508bfc8db5d
pablo <pablo.diazv@pucp.edu.pe>
2021-02-23 23:41:29 -0500

    cadmun

--- build/release.sh
+++ build/release.sh
@@ -13,36 +13,49 @@ then
 	echo "Remove target/ubuntu directory before running, it's probably owned by root due to docker"
 	exit 1
 fi
+
+# STEP 1: Clone repo and Cmake
+echo '========== START: CLONING REPO AND CMAKE =========='
 git clone https://github.com/werman/noise-suppression-for-voice.git
 cd noise-suppression-for-voice
 cmake -Bbuild-x64 -H. -DCMAKE_BUILD_TYPE=Release
 cd build-x64
 make
-\cp bin/ladspa/librnnoise_ladspa.so ../../src/main/resources/base/librnnoise_ladspa.so
+cp bin/ladspa/librnnoise_ladspa.so ../../src/main/resources/base/librnnoise_ladspa.so
 cd ../../
 rm -rf noise-suppression-for-voice
-
+mkdir releases/
 mkdir releases/$1
+echo '========== END: CLONING REPO AND CMAKE =========='
 
-# build FBS VM
-fbs buildvm ubuntu
+# STEP 2: Build FBS VM
+echo '========== START: BUILD FBS VM =========='
+python3.6 -m pip install -r requirements.txt
+sudo fbs buildvm fedora
 
-# build docker run command identical to fbs, but inject our own bashrc
+echo '========== END: BUILD FBS VM =========='
+
+# STEP 3: Build docker run command identical to fbs, but inject our own bashrc
+echo '========== START: BUILD DOCKER IDENTICAL FBS BUT WITH BASHRC =========='
 docker_run="docker run -it"
 for i in `ls -A | grep -v target` ; do
- docker_run="$docker_run -v `readlink -f $i`:/root/cadmus/$i"
+  docker_run="$docker_run -v `readlink -f $i`:/root/cadmus/$i"
 done
 docker_run="$docker_run -v `readlink -f ./target/ubuntu`:/root/cadmus/target -v `readlink -f ./build/resources/.bashrc`:/root/.bashrc cadmus/ubuntu "
 echo $docker_run
 eval $docker_run
+echo '========== END: BUILD DOCKER IDENTICAL FBS BUT WITH BASHRC =========='
 
-# copy artifacts to released directory
+# STEP 4: Copy artifacts to released directory
+echo '========== START: COPY ARTIFACTS TO RELEASE DIRECTORY =========='
 cp target/ubuntu/cadmus.deb releases/$1
 cd target/ubuntu
 zip -r ../../releases/$1/cadmus.zip cadmus
 cd ../../
+echo '========== END: COPY ARTIFACTS TO RELEASE DIRECTORY =========='
 
-# build AppImage
+# STEP 5: Build AppImage
+echo '========== START: BUILD APPIMAGE =========='
 cd build/resources
 git clone https://github.com/AppImage/pkg2appimage.git
 cp Cadmus.yml ./pkg2appimage/recipes
@@ -53,5 +66,6 @@ cp out/* ../../../releases/$1/cadmus.AppImage
 cd ..
 rm -rf pkg2appimage
 echo "Release artifacts are in: ../releases/$1"
+echo '========== END: BUILD APPIMAGE =========='
 
 # todo: create GitHub release & upload artifacts
